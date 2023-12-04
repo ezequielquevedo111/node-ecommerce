9f8308a4cc1b5bdcf1bf739ea46946732034b9d4
Ezequiel Quevedo <ezequielquevedo111@gmail.com>
2023-12-04 13:51:44 -0300

    primer commit

---
+++ main/index.js
@@ -0,0 +1,18 @@
+//Array products//
+const products = [];
+
+//Agregar products//
+const addProduct = (product) => {
+  products.push(product);
+};
+
+//Buscar por id//
+const searchById = () => {};
+
+//Actualizar por id//
+
+const updateById = () => {};
+
+// Remover por id//
+
+const removeById = () => {};
