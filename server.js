27b9ac695cd6330d7aa2d38d404c008e087d68e6
Ezequiel Quevedo <ezequielquevedo111@gmail.com>
2024-01-11 20:33:50 -0300

    hbs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./src/routers/index.router.js";
+import { engine } from "express-handlebars";
 import errorHandler from "./src/middlewares/errorHandler.js";
 import pathHandler from "./src/middlewares/pathHandler.js";
 import __dirname from "./utils.js";
@@ -11,11 +12,17 @@ const server = express();
 const PORT = 8080;
 const ready = () => console.log("server ready on PORT: " + PORT);
 
+//Templates//
+server.engine("handlebars", engine());
+server.set("view engine", "handlebars");
+server.set("views", __dirname + "/src/views");
+
 //Middlewares//
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(__dirname + "/public"));
 server.use(morgan("dev"));
+
 server.use("/", router);
 server.use(errorHandler);
 server.use(pathHandler);
